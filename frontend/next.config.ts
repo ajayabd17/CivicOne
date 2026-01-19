f32cec8d916b1d9e66bd41e6275d6d0eca3bee75
Ajay Dev Pavithran <ajayabd10@gmail.com>
2026-01-20 04:35:33 +0530

    vercel config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,4 @@
 import type {NextConfig} from 'next';
-import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -8,13 +7,6 @@ const nextConfig: NextConfig = {
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './src'),
-    };
-    return config;
   },
   images: {
     remotePatterns: [
